e947b4f2f721e632da9ef6b9254aab1829878654
houwenhao4147 <houwenhao4147@163.com>
2017-07-23 07:35:35 +0800

    2 commit

--- app/src/main/java/com/hou/gitdemo/MainActivity.java
+++ app/src/main/java/com/hou/gitdemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("TAG","MAINACTIVITY");
+		 Log.e("TAG","MAINACTIVITY2");
     }
 }
