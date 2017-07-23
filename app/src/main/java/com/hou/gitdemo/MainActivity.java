96d87bbceb6999d2eaa5db23e45750bb0b37842a
houwenhao4147 <houwenhao4147@163.com>
2017-07-23 17:35:59 +0800

    555 commit

--- app/src/main/java/com/hou/gitdemo/MainActivity.java
+++ app/src/main/java/com/hou/gitdemo/MainActivity.java
@@ -21,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Log.e("TAG","MAINACTIVITY131224444444");
 
 	    Log.e("TAG","MAINACTIVITY131223333");
+		
+		Log.e("TAG","MAINACTIVITY13122333355555");
     }
 }
